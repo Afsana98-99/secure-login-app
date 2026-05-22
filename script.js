e4d113c43b865c1427337c2a5234dcb6b467888a
Afsana98-99 <nadafafsana04@gmail.com>
2026-05-22 12:10:38 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  alert("Login Successful!");
+});
+
+function logout() {
+  alert("Logged Out!");
+}
